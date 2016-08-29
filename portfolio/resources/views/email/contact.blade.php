59e6ad1f5de8e2cfc774b92cb083deb9f18d625a
Kamil <kamil.mach95@gmail.com>
2016-08-29 14:38:35 +0200

    test

--- portfolio/resources/views/email/contact.blade.php
+++
@@ -1,9 +0,0 @@
-<h3>NOWA WIADOMOŚĆ Z FORMUALRZA KAMIL-MACH.PL</h3>
-
-<div>
-	<span style="font-weight:bold;">Imie: </span>{{$name}} <br />
-	<span style="font-weight:bold;">Adres e-mail: </span>{{$email}} <br />
-	<span style="font-weight:bold;">Temat: </span>{{$subject}} <br />
-	<span style="font-weight:bold;">Treść wiadomości: </span>{{$bodyMessage}} <br />
-</div>
-
